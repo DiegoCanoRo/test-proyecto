02dd1bacc70d43c0f8847764c590f59c9e1fefaf
DiegoCanoRo <141886478+DiegoCanoRo@users.noreply.github.com>
2025-05-12 16:16:59 -0700

    Add files via upload

--- TestASM.java
+++ TestASM.java
@@ -4,5 +4,8 @@ public class TestASM {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        
+        
+        System.out.println("aosdiawd");
     }
 }
